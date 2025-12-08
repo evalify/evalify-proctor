562a25908a127b5b273bfd2e90d1f0e9c70abcec
TharunCodes07 <senthietk@gmail.com>
2025-12-08 16:25:17 +0530

    fix : unused warnings on platforms

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -2,8 +2,11 @@ use async_trait::async_trait;
 use anyhow::Result;
 use tokio::process::Child;
 
+#[cfg(target_os = "windows")]
 pub mod windows;
+#[cfg(target_os = "linux")]
 pub mod linux;
+#[cfg(target_os = "macos")]
 pub mod mac;
 
 
